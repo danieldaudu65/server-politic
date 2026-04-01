32cd59f51273f534e34459a3f8436192bae1e221
danieldaudu65 <danieldaudu65@gmail.com>
2026-04-01 09:09:28 +0100

    git

--- models/Admin.js
+++ models/Admin.js
@@ -11,6 +11,8 @@ const adminSchema = new mongoose.Schema({
 adminSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
+
+  
   this.password = await bcrypt.hash(this.password, 10);
 });
 
